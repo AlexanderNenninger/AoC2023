d2377436b89cda3530316ed845bc5d4664990216
Alexander Nenninger <alexander.nenninger@nttdata.com>
2023-12-09 11:25:42 +0100

    initial commit

---
+++ src/days/day20.rs
@@ -0,0 +1,10 @@
+use crate::{Solution, SolutionPair};
+
+///////////////////////////////////////////////////////////////////////////////
+
+pub fn solve() -> SolutionPair {
+    let sol1: u64 = 0;
+    let sol2: u64 = 0;
+
+    (Solution::U64(sol1), Solution::U64(sol2))
+}
---
+++ src/etc/mod.rs
@@ -0,0 +1,4 @@
+pub mod solution;
+
+pub use solution::ErasedError;
+pub use solution::Solution;
